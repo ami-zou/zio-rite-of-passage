d3bb592f6322c63bf3832ae29f0067a523496c4e
Ami <azou@ripple.com>
2024-07-29 12:51:15 -0400

    init

---
+++ modules/server/src/test/scala/com/rockthejvm/reviewboard/repositories/CompanyRepositorySpec.scala
@@ -0,0 +1,8 @@
+package com.rockthejvm.reviewboard.repositories
+
+import zio.*
+import zio.test.*
+
+object CompanyRepositorySpec extends ZIOSpecDefault {
+  override def spec: Spec[TestEnvironment with Scope, Any] = ???
+}
